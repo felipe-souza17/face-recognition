ef04d62748dae0eeb8134178d4a83e93df42ed2e
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-07-02 12:50:03 -0300

    Detecção através de face

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,68 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as faceapi from 'face-api.js'
 import './App.css'
 
 function App() {
-  const imgRef = useRef()
+  const videoRef = useRef()
   const canvasRef = useRef()
+  const [initializing, setInitializing] = useState()
 
-  const handleImage = async () => {
-    const detections = await faceapi
-      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions()
+  const handleVideo = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+    videoRef.current.srcObject = stream
 
-    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current)
-    faceapi.matchDimensions(canvasRef.current, {
-      height: 750,
-      width: 476
-    })
-    const resized = faceapi.resizeResults(detections, {
-      height: 750,
-      width: 476
-    })
-    faceapi.draw.drawDetections(canvasRef.current, resized)
-    faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
+    // canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(
+    //   videoRef.current
+    // )
+    // faceapi.matchDimensions(canvasRef.current, {
+    //   height: 750,
+    //   width: 476
+    // })
+    // const resized = faceapi.resizeResults(detections, {
+    //   height: 750,
+    //   width: 476
+    // })
+    // faceapi.draw.drawDetections(canvasRef.current, resized)
+    // faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
   }
 
   useEffect(() => {
     const loadModels = () => {
+      setInitializing(true)
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models')
       ])
-        .then(handleImage)
+        .then(handleVideo)
         .catch(e => console.log(e))
     }
-    imgRef.current && loadModels()
+    loadModels()
   }, [])
+
+  const handleVideoOnPlay = () => {
+    // setInterval(async () => {
+    //   if (initializing) {
+    //     setInitializing(false)
+    //   }
+    //   const detections = await faceapi
+    //     .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+    //     .withFaceLandmarks()
+    //     .withFaceExpressions()
+    //   console.log(detections)
+    // }, 1000)
+  }
   return (
     <div className="app">
-      <img
-        crossOrigin="anonymous"
-        ref={imgRef}
-        src="https://images.pexels.com/photos/16853616/pexels-photo-16853616/free-photo-of-adulto-ramalhete-buque-crisantemo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="Imagem de teste"
+      <span>{initializing ? 'Inicializando modelos' : 'Pronto'}</span>
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        height="750"
+        width="476"
+        onPlay={handleVideoOnPlay}
       />
       <canvas ref={canvasRef} height="750" width="476" />
     </div>
